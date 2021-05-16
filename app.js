7da74d9622de6eee37ab76523a6303880a285ce6
sergiospac <68155552+sergiospac@users.noreply.github.com>
2021-05-16 17:32:45 -0300

    Commit inicial do Avatar
    É para a Pri pegar e continuar o projeto para estudar.

---
+++ app.js
@@ -0,0 +1,65 @@
+
+const addInfo = async avatarList => {
+  const getAvatarByIdUrl = id => `https://last-airbender-api.herokuapp.com/api/v1/characters/${id}`
+  const newList = await avatarList.map(async element => {
+    const id = element._id
+    const avatarByIdUrl = getAvatarByIdUrl(id)
+    const result = await fetch(avatarByIdUrl)
+    const avatarInfo = await result.json()
+
+    element.first = avatarInfo.first
+    element.gender = avatarInfo.gender
+    element.hair = avatarInfo.hair
+    element.love = avatarInfo.love
+    element.position = avatarInfo.position
+    element.predecessor = avatarInfo.predecessor
+    element.profession = avatarInfo.profession
+    element.weapon = avatarInfo.weapon
+
+    return element
+  })
+
+  return newList
+}
+
+const getAvatars = async () => {
+  //const allUrl = "https://last-airbender-api.herokuapp.com/api/v1/characters?perPage=500&page=1"
+  const avatarUrl = "https://last-airbender-api.herokuapp.com/api/v1/characters?profession=Avatar"
+
+  const result = await fetch(avatarUrl)
+  let avatarList = await result.json()
+
+  newAvatarList = await addInfo(avatarList)
+
+  return newAvatarList
+}
+
+getAvatars().then(async avatars => {
+
+  const list = avatars.reduce(async (accumulator, aAvatar) => {
+
+    const avatar = await aAvatar
+    let acc = await accumulator
+
+    const type = avatar.affiliation ?? '- - -'
+    const enemies = avatar.enemies
+
+    acc += `
+    <li class "card ${type}" >
+    <img class="card-img" alt="${avatar.name}" src="${avatar.photoUrl}">
+    <h2 class="card-title">Nome: ${avatar.name}</h2>
+    <h3 class="card-subtitle">Inimigos: ${enemies ?? "- - -"}</h3>
+    <p>Arma: ${avatar.weapon}</p>
+    <p>Profissão: ${avatar.profession}</p>
+    <p>Afiliação: ${type}</p>
+    </li>
+    `
+
+    return acc
+  }, '')
+
+  const ul = document.querySelector('[data-js="avatar"]')
+  ul.innerHTML = await list
+
+})
+
